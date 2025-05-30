5aaa9db40d13d74fc35dc891cf77bf58e90361c6
Estatic24 <130591676+Estatic24@users.noreply.github.com>
2025-05-31 00:16:39 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,8 @@ require('./controllers/commentController')(wss, Comment)
 
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
